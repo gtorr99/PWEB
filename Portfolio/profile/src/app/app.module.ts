9a93cf921bf16d41039d0c12110cbcc128417e1c
gtorres17 <gtorres89@outlook.com.br>
2020-11-02 19:37:05 -0300

    App component configurado

--- Portfolio/profile/src/app/app.module.ts
+++ Portfolio/profile/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalDataComponent } from './personal-data/personal-data.component';
+import { SlideComponent } from './slide/slide.component';
+import { ContentComponent } from './content/content.component';
+import { SlideService } from './slide-service/slide.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalDataComponent,
+    SlideComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [SlideService, ContentComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
