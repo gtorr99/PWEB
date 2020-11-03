7231f1020cc1c877ac2b0e44d19193074a0e144e
gtorres17 <gtorres89@outlook.com.br>
2020-11-02 22:06:00 -0300

    Melhoria no slide service

--- Portfolio/profile/src/app/slide-service/slide.service.ts
+++ Portfolio/profile/src/app/slide-service/slide.service.ts
@@ -6,10 +6,13 @@ export class SlideService{
     incrementIndex(){
         if(this.index < this.MAX)
             this.index++;
+        else
+            this.index = 0;
     }
 
     decrementIndex(){
         if(this.index > 0)
             this.index--;
+
     }
 }
